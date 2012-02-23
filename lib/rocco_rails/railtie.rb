d98fee2f0755de6a2a68a34c370a3c019553c017
Mawi Marín <mawitu@gmail.com>
2012-02-23 20:18:51 +0100

    Removed railtie_name. fixes #1

--- lib/rocco_rails/railtie.rb
+++ lib/rocco_rails/railtie.rb
@@ -4,7 +4,7 @@ require 'rocco_rails'
 require 'rails'
 module RoccoRails
   class Railtie < Rails::Railtie
-    railtie_name :rocco_rails
+    # railtie_name :rocco_rails
 
     rake_tasks do
       load "tasks/rails_tasks.rake"
